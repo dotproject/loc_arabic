bb0cd774f4e2d572e7c7c105f7d51ea5a6421cc6
Adam Donnison <ajdonnison@gmail.com>
2024-11-02 15:49:41 +1100

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Arabic Locale and Style for dotProject
+This was copied from the old dotProject downloads site.  It may be incomplete.
---
+++ locales/ar/locales.php
@@ -0,0 +1,7 @@
+<?php
+//$locale_char_set = 'windows-1256';
+$locale_char_set = 'utf-8';
+// 0 = sunday, 1 = monday
+define( 'LOCALE_FIRST_DAY', 6 );
+define( 'LOCALE_TIME_FORMAT', '%I:%M %p');
+?>
